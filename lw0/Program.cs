bebc31c08593414755f5f30193592afa006363eb
Thhexo <123793886+Thhexo@users.noreply.github.com>
2023-02-14 18:50:14 +0300

    Initial commit

---
+++ lw0/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int e;
+Console.WriteLine("Введите число: ");
+e = Convert.ToInt32(Console.ReadLine());
+if((e%3==0)&&(e%5==0)){
+Console.WriteLine("fizzbuzz");
+}else if(e%5==0){
+Console.WriteLine("buzz");
+}else if(e%3==0){
+Console.WriteLine("fizz");
+}
