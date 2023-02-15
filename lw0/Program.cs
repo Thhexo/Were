82cfac1b81749e19c215ab51e05f1e94c325e8b4
Thhexo <123793886+Thhexo@users.noreply.github.com>
2023-02-15 22:34:23 +0300

    fixed program

--- lw0/Program.cs
+++ lw0/Program.cs
@@ -1,11 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-int e;
-Console.WriteLine("Введите число: ");
-e = Convert.ToInt32(Console.ReadLine());
-if((e%3==0)&&(e%5==0)){
-Console.WriteLine("fizzbuzz");
-}else if(e%5==0){
-Console.WriteLine("buzz");
-}else if(e%3==0){
-Console.WriteLine("fizz");
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int e;
+            Console.WriteLine("Введите число: ");
+            e = Convert.ToInt32(Console.ReadLine());
+            if ((e % 3 == 0) && (e % 5 == 0))
+            {
+                Console.WriteLine("fizzbuzz");
+            }
+            else if (e % 5 == 0)
+            {
+                Console.WriteLine("buzz");
+            }
+            else if (e % 3 == 0)
+            {
+                Console.WriteLine("fizz");
+            }
+        }
+    }
 }
