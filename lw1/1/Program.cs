083fdcd9b393319247fce378c5e628589b26b656
Thhexo <123793886+Thhexo@users.noreply.github.com>
2023-03-03 22:25:32 +0300

    1 is fine

---
+++ lw1/1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, p, l;
+            Console.WriteLine("Сколько элементов в вашей последовательности?");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] b = new int[n];
+            for (int i = 0; i < n;)
+            {
+                Console.WriteLine("Ведите " +i+ " элемент последовательности");
+                l = Convert.ToInt32(Console.ReadLine());
+                b[i] = l;
+                i++;
+            };
+
+            Console.WriteLine("Ведите p");
+            p = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Следующие элементы последовательности больше " +p );
+            foreach(int posl in b)
+            {
+                if ( posl> p)
+                {
+                    Console.WriteLine(posl);
+                }
+                else{
+                    continue;
+                }
+
+            }
+        }
+    }
+}
