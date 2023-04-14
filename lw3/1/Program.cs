af718f3a93492bec0885826675e09e183209c55c
Thhexo <123793886+Thhexo@users.noreply.github.com>
2023-04-14 22:51:00 +0300

    initial commit

---
+++ lw3/1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static  int [] Razvorot(int[] numbers, int n)
+        { int [] numberraz= new int[n];
+            for (int i = n - 1, e=0; i > (-1); i--,e++)
+            {
+               numberraz[e]=numbers[i];
+            }
+            return numberraz;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Сколько элементов в вашем массиве?");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[n];
+            
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Ведите {0} элемент массива", i);
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Введенный массив в обратном порядке [{0}]", string.Join(", ", Razvorot(numbers, n)));
+        }
+    }
+}
---
+++ lw3/2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static int razrad(int number)
+        {
+            string number1 = Convert.ToString(number);
+            int lengthnumber = Convert.ToInt32(number1.Length);
+            return lengthnumber;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Число {number} содержит в себе {razrad(number)} разрядов");
+        }
+    }
+}
+
