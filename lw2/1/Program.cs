19c2bf72b6b27bb80a10958566649f15f4e31de5
Thhexo <123793886+Thhexo@users.noreply.github.com>
2023-05-05 23:53:46 +0300

    Initial commit

---
+++ lw2/1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 1 предложение");
+            string one = Console.ReadLine();
+            Console.WriteLine("Введите 2 предложение");
+            string two = Console.ReadLine();
+            string[] wordOne = one.Split(new char[] { ' ' });
+            string[] wordTwo = two.Split(new char[] { ' ' });
+
+            int[] massword = new int[wordOne.Length];
+            int i = 0, n;
+            foreach (string o in wordOne)
+            {
+                n = 0;
+                foreach (string t in wordTwo)
+                {
+                    if (o == t) massword[i] = ++n;
+                }
+                i++;
+            }
+
+            Console.WriteLine("Во втором предложении повторялись следующие слова:");
+            for (int v = 0; v < wordOne.Length; v++)
+            {
+                Console.WriteLine("{0}={1}", wordOne[v], massword[v]);
+            }
+        }
+    }
+}
---
+++ lw2/2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static string noSpase(string one)
+        {
+            for (int i = 0; i < one.Length; i++)
+            {
+                while (one[i] == ' ' && one[i + 1] == ' ')
+                {
+                    one = one.Remove(i + 1, 1);
+                }
+            }
+            return one;
+        }
+        static void wordMax(string[] mass)
+        {
+            int maxWord = mass[0].Length;
+            int indexMax = 0;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (mass[i].Length > maxWord)
+                {
+                    indexMax = i;
+                    maxWord = mass[i].Length;
+                }
+            }
+
+            Console.WriteLine("Самое длинное слово: " + mass[indexMax]);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string one = Console.ReadLine();
+
+            string oneNoSpase = noSpase(one);
+
+            string[] mass = oneNoSpase.Split(new char[] { ' ' });
+
+            wordMax(mass);
+
+
+        }
+    }
+}
